e87004f237514cdfaa75e5a7f3d9f8e1a1949a8d
--replace-all <victor1996d@gmail.com>
2021-11-15 13:33:36 +0100

    index.html : templates

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
@@ -16,3 +18,12 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)    
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))    
