2882b193bd01c7735970af29ee7b0bc8ea8b4860
--replace-all <victor1996d@gmail.com>
2021-11-15 18:48:48 +0100

    index : views.py

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
+from django.shortcuts import render
 
 
 def index(request):
@@ -22,8 +23,7 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))    
+    return render(request, 'polls/index.html', context)    
