b785582b4eef60e2c7b64f909eb015f381995021
--replace-all <victor1996d@gmail.com>
2021-11-29 11:28:15 +0100

    get_queryset(modified) : views.py, QuestionModelTests : tests.py

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -11,8 +12,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions.(not including those set to be
+    published in the future)"""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
